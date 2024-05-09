1e80d9df3723990423c93c232570fd327bc68924
gshvedin <77393847+gshvedin@users.noreply.github.com>
2024-05-09 23:33:21 +0200

    Update JsonExtensions.cs

--- WorkflowEngine/Helpers/JsonExtensions.cs
+++ WorkflowEngine/Helpers/JsonExtensions.cs
@@ -1,40 +1,71 @@
-﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Data;
 
 namespace WorkflowEngine.Helpers
 {
     public static class JsonExtensions
     {
-        public static JObject TryAddByPath(this JObject obj, string path, JToken newValue)
+
+        public static JObject AddByPath(this JObject obj, string path, JToken newValue)
         {
             if (string.IsNullOrEmpty(path))
             {
                 return obj;
             }
 
-            if (path.Contains("[0]", System.StringComparison.InvariantCulture))
+            // Normalize the path by removing specific notations used in JSON paths
+            if (path.Contains("[0]"))
             {
-                // escaping zeroindexer
-                path = path.Replace(".[0].", ".", System.StringComparison.InvariantCulture).Replace("[0].", ".", System.StringComparison.InvariantCulture);
+                // Remove the zero indexer notation to simplify path handling
+                path = path.Replace(".[0].", ".").Replace("[0].", ".");
+            }
+            if (path.Contains("$."))
+            {
+                // Remove the root object notation
+                path = path.Replace("$.", "");
             }
 
-            // slice path
+            // Split the path into parts
             string[] slices = path.Split('.');
 
             // if there no slices just add value, othervise add nested elements recursively
             if (slices.Length == 1)
             {
-                if (!string.IsNullOrEmpty(path))
+                if (!string.IsNullOrEmpty(path) && !obj.ContainsKey(path))
                 {
-                    obj.TryAdd(path, newValue);
+                    obj.Add(path, newValue);
                 }
             }
             else
             {
-                obj.TryAdd(slices[0], new JObject().TryAddByPath(path.Replace($"{slices[0]}.", string.Empty, System.StringComparison.InvariantCulture), newValue));
+                if (!obj.ContainsKey(slices[0]))
+                {
+                    obj.Add(slices[0], new JObject());
+                }
+                JObject nestedObj = (JObject)obj[slices[0]];
+                nestedObj.AddByPath(path.Replace($"{slices[0]}.", string.Empty), newValue);
             }
 
             return obj;
         }
+
+
+        public static JObject AddOrReplaceByPath(this JObject obj, string path, JToken newValue)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return obj;
+            }
+
+            if (obj.SelectToken(path) is JToken token)
+            {
+                token.Replace(newValue); // Direct assignment replaces the existing value
+                return obj;
+            }
+
+            return obj.AddByPath(path, newValue);
+        }
     }
 }
+
